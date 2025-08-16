3487b33efa437333fa7cf5b94eaffa96076122ce
Fabian Sanchez <fabiansanchezd@outlook.com>
2025-08-15 20:24:29 -0600

    transaction built

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,16 +1,32 @@
-import Image from "next/image";
+'use client'
+
+import MakeTx from "../../transactions/makeTx";
+import { useState } from "react";
 
 export default function Home() {
+  const [status, setStatus] = useState("");
+
   return (
     <div className="font-sans font-bold flex flex-col items-center justify-items-center min-h-screen p-8 py-20 gap-5">
         <h1 className="text-4xl">Tip Jar (Testnet)</h1>
         <h2 className="text-2xl">Send a tip to: <span className="text-3xl text-blue-500">{process.env.NEXT_PUBLIC_RECEIVER_NAME}</span></h2>
         <h2 className="text-l">Receiver address: <span className="text-l text-blue-400">{process.env.NEXT_PUBLIC_RECEIVER_PK}</span></h2>
-        <form action="" className="flex flex-col gap-3 pt-3">
+        <form action="" 
+        onSubmit={async (e) => {
+              e.preventDefault();
+              const form = e.currentTarget;
+              console.log(form);
+              const amount = (form.elements[0] as HTMLInputElement).value;
+              const memo = (form.elements[1] as HTMLInputElement).value;
+              const result = await MakeTx(amount, memo);
+              setStatus(result);
+            }}
+        className="flex flex-col gap-3 pt-3">
             <input type="text" placeholder="Enter your tip amount in XLMs" className="border border-gray-300 p-2 rounded" />
             <input type="text" placeholder="Enter your message (memo)" className="border border-gray-300 p-2 rounded" />
             <button type="submit" className="bg-blue-500 text-white p-2 rounded">Tip with Freighter</button>
         </form>
+        {status != "" ? <p>Status: {status}</p> : null}
     </div>
   );
 }
---
+++ frontend/transactions/makeTx.js
@@ -0,0 +1,67 @@
+import {
+  Horizon,
+  TransactionBuilder,
+  Operation,
+  Asset,
+  Networks,
+  Memo,
+} from '@stellar/stellar-sdk';
+import * as Freighter from '@stellar/freighter-api';
+
+const server = new Horizon.Server('https://horizon-testnet.stellar.org'); // we are working on testnet
+const receiverPublicKey= process.env.NEXT_PUBLIC_RECEIVER_PK;
+
+const makeTx = async (amount, memo) => {
+  const checkConnection = await Freighter.isConnected();
+  if (!checkConnection) {
+    return "Freighter is not connected";
+  }
+
+  const isAppAllowed = await Freighter.isAllowed();
+
+  if (!isAppAllowed.isAllowed) {
+    const allowApp = await Freighter.setAllowed();
+  }
+
+  const addressFreighter = await Freighter.getAddress();
+
+  // we do this cause not entirely sure if addressFreighter is always an object or a string
+  const publicKey = addressFreighter.address ?? addressFreighter; 
+  if (typeof publicKey !== 'string' || !publicKey) {
+    return "Failed to get public key";
+  }
+
+  // we have to load the account from with horizon
+  const account = await server.loadAccount(publicKey);
+
+  const tx = new TransactionBuilder(account, {
+    fee: String(await server.fetchBaseFee()),
+    networkPassphrase: Networks.TESTNET,
+  })
+  .addOperation(Operation.payment({
+      destination: receiverPublicKey,
+      asset: Asset.native(),
+      amount,
+    }))
+    .setTimeout(60)
+    .addMemo(Memo.text(memo.slice(0,28)))
+    .build();
+
+  const xdr = tx.toXDR(); // we need to pass it to xdr for it to work with freighter
+
+  const signature = await Freighter.signTransaction(xdr, { networkPassphrase: Networks.TESTNET });
+  if (!signature) {
+    return "Failed to sign transaction";
+  }
+  const signedTx = TransactionBuilder.fromXDR(xdr, Networks.TESTNET);
+
+  const res = await server.submitTransaction(signedTx);
+  if (res instanceof Error) {
+    return "Failed to submit transaction";
+  } else {
+    return `Transaction submitted succesfully: ${res.hash}`;
+  }
+
+}
+
+export default makeTx;
